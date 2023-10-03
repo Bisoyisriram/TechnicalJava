3b25f7454f837387eb920e7b7f3abb93ed6ee157
Srirambisoyi <bisoyisriram@gmail.com>
2023-10-04 01:04:53 +0530

    First commit

---
+++ Technical_trainng/src/Technical_trainng/hello.java
@@ -0,0 +1,19 @@
+package Technical_trainng;
+
+public class hello {
+    void add()
+    {
+        int b=90;
+        System.out.println(b);
+    }
+
+    {
+        int b=25;
+        System.out.println(b);
+    }
+    public static void main(String[] args) {
+        hello o=new hello();
+        o.add();
+    }
+
+}
